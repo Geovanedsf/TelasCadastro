ed7b18a9d7ad725b3fafab3b3c248f1d0b762578
Geovane Freitas <geovanefreitas@MacBook-de-Geovane.local>
2023-01-02 14:05:11 -0300

    Ajustes layout

---
+++ app/src/main/java/com/example/registerproject/Model/Users.kt
@@ -0,0 +1,21 @@
+package com.example.registerproject.Model
+
+data class User(
+
+    var nome: String,
+    var email: String,
+    var cpf: String,
+    var rg: String,
+    var tel: String,
+    var nascdate: String,
+    var cep: String,
+    var nationality: String,
+    var state: String,
+    var city: String,
+    var district: String,
+    var street: String,
+    var streetnumber: String,
+    var complement: String,
+    val senha: String,
+
+)
